dcd0c6d80421b28cd22d5573ee9ee7a1ff64e98f
lemarpopal <19434803+lemarpopal@users.noreply.github.com>
2021-04-05 16:41:15 -0700

    update sentinel automation script and add readme and config file

---
+++ data-prep/Sentinel/automate/automation.py
@@ -0,0 +1,283 @@
+from hyp3_sdk import HyP3
+from osgeo import gdal
+from configparser import ConfigParser
+import pandas as pd
+import datetime
+
+import boto3
+from botocore.exceptions import ClientError
+
+import os
+import concurrent.futures
+import ntpath
+import time
+import subprocess
+
+import re
+
+from copy import copy
+from pathlib import Path
+import numpy as np
+
+# TODO: Use global variables for things like s3, bucket_name?
+
+def main():
+    config_file = 'config.ini'
+    config = ConfigParser()
+    config.read(config_file)
+    
+    aws_access_key_id = config['aws']['aws_access_key_id']
+    aws_secret_access_key = config['aws']['aws_secret_access_key']
+    prefix_str = config['aws']['prefix_str']
+    dest_bucket = config['aws']['dest_bucket']
+    max_threads = int(config['misc']['max_threads'])
+    
+    hyp3 = HyP3(username=config['HyP3']['username'], password=config['HyP3']['password'])
+    s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    granules_group_dict, start_date = submit_granules(config['csv']['csv'])
+
+    for year_path_frame in granules_group_dict.keys():
+        year, path_frame = year_path_frame.split('_', 1)
+
+        print(year, path_frame, "copying jobs to bucket")
+        copy_granules_to_bucket(hyp3, s3, prefix_str, dest_bucket, year, path_frame, start_date)
+        print(year, path_frame, "DONE copying jobs to bucket")
+
+        print(year, path_frame, "building vrt and uploading to s3")
+        VV_VRT_filename, VH_VRT_filename = build_vrt_and_upload_to_s3(s3, dest_bucket, prefix_str, year, path_frame)
+        print(year, path_frame, "DONE building vrt and uploading to s3")
+    
+    # use threading to prevent entire script crashing if one operation throws an error
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        print(year, path_frame, "calc temp avg and uploading to s3")
+        futures = []
+        for year_path_frame,granule_name_list in granules_group_dict.items():
+            futures.append(executor.submit(calc_temp_avg_and_upload_to_s3, s3, dest_bucket, prefix_str, year, path_frame, VV_VRT_filename, VH_VRT_filename))
+
+        for fut in futures:
+            print(fut.result())
+        executor.shutdown(wait=True) # wait for all threads to finish
+        print(year, path_frame, "DONE calc temp avg and uploading to s3")
+
+    print("done with everything")
+
+"""
+Submits the granules to Hyp3 and returns the granules dictionary and the date (today) they were submitted. 
+"""
+def submit_granules(csv_path, hyp3):
+    granules_df = pd.read_csv(csv_path)
+    granules_df['Year'] = granules_df['Acquisition Date'].apply(lambda x: x.split('-')[0])
+    granules_df = granules_df.filter(['Granule Name','Year','Path Number','Frame Number'])
+    granules_df['year_path_frame'] = granules_df.apply(lambda row: "{}_{}_{}".format(row['Year'], row['Path Number'], row['Frame Number']), axis=1)
+
+    granules_groups = granules_df.groupby(by=['year_path_frame'])['Granule Name']
+
+    # create a dictionary
+    # key example: 'year_path_frame' --> '2018_25_621'
+    # value is a list of granule names that have the year,path,frame in the key
+    granules_group_dict = {
+        key : granules_groups.get_group(x).to_list()
+        for key,x in zip(granules_groups.indices,granules_groups.groups)
+    }
+
+    # TODO: Make start date today's date
+    start_date = datetime.datetime(2021,3,9) # the date the jobs were submitted
+    # submit the jobs for all year_path_frame's
+    for year_path_frame,granule_name_list in granules_group_dict.items():
+        for granule_name in granule_name_list:
+                hyp3.submit_rtc_job(granule_name, year_path_frame, resolution=30.0, radiometry='gamma0',
+                                    scale='power', speckle_filter=False, dem_matching=True,
+                                    include_dem=True, include_inc_map=True, include_scattering_area=True)
+
+    return granules_group_dict, start_date
+
+def copy_granules_to_bucket(hyp3, s3, prefix_str, dest_bucket, year, path_frame, start_date):
+    year_path_frame = "_".join([year, path_frame])
+
+    batch = hyp3.find_jobs(name=year_path_frame, start=start_date) # only get jobs after today's date
+    batch = hyp3.watch(batch)
+    
+    for job in batch.jobs:
+        copy_source = {
+            'Bucket':job.files[0]['s3']['bucket'],
+            'Key':job.files[0]['s3']['key']
+        }
+        filename = ntpath.basename(job.files[0]['s3']['key'])
+        destination_key = os.path.join(prefix_str, '{}/{}/{}'.format(year, path_frame, filename))
+        s3.meta.client.copy(copy_source, Bucket=dest_bucket, Key=destination_key)
+
+def build_vrt_and_upload_to_s3(s3, dest_bucket, prefix_str, year, path_frame):
+
+    VV_VRT_filename, VH_VRT_filename = build_vrt(s3, dest_bucket, prefix_str, year, path_frame)
+
+    VV_VRT_s3_key = os.path.join(prefix_str, year, path_frame, VV_VRT_filename)
+    VH_VRT_s3_key = os.path.join(prefix_str, year, path_frame, VH_VRT_filename)
+
+    upload_to_s3(s3, VV_VRT_filename, dest_bucket, VV_VRT_s3_key)
+    upload_to_s3(s3, VH_VRT_filename, dest_bucket, VH_VRT_s3_key)
+
+    return VV_VRT_filename, VH_VRT_filename
+
+def build_vrt(s3, bucket_str, prefix_str, year, path_frame):
+    # if the bucket has more files that we don't care about, use this to filter only files that start with the given prefix string
+    bucket_object = s3.Bucket(bucket_str)
+
+    VV_list = []
+    VH_list = []
+    
+    folder = os.path.join(prefix_str, year, path_frame)
+    for file in bucket_object.objects.filter(Prefix=folder):
+        if file.key.endswith('.zip'):
+            VV_list.append(generate_tif_full_filename(bucket_str, file.key, "VV"))
+            VH_list.append(generate_tif_full_filename(bucket_str, file.key, "VH"))
+
+    # TODO: any other kwargs we're missing? targetAlignedPixels?
+    VV_VRT_filename = "{}_{}_VV.vrt".format(year, path_frame)
+    gdal_build_vrt(VV_VRT_filename, VV_list)
+
+    VH_VRT_filename = "{}_{}_VH.vrt".format(year, path_frame)
+    gdal_build_vrt(VH_VRT_filename, VH_list)
+    
+    # prevent race condition where we attempt to upload the files to s3 before gdal creates the vrt
+    block_until_file_created(VV_VRT_filename)
+    block_until_file_created(VH_VRT_filename)
+
+    # returns the keys to the VV and VRT files in S3
+    return VV_VRT_filename, VH_VRT_filename
+
+"""
+Given the name of a .zip file in an AWS bucket, generates the full filename
+of the corresponding .tif file inside the .zip file that we want to build the
+VRT with in a format that GDAL understands.
+Polarization should either be "VV" or "VH"
+"""
+def generate_tif_full_filename(bucket, filename, polarization):
+    # TODO: Make this a parameter. 
+    # Hardcoded variable controls whether we are building links for S3 or local filesystem
+    cloud = True
+    local_path = "/data3/lepopal_vault/datasets/2018_data"
+
+    regex = re.compile(r"""
+                    (.*[/\\])?   # matches any folder names before the granule name
+                    (.+)         # matches the granule name
+                    (?:\.zip)    # matches the .zip file extension
+                    """, re.VERBOSE)
+
+    match = re.search(regex, filename)
+    if not match:
+        raise ValueError("unrecognized filename format: " + filename)
+
+    folder = match.group(1)
+    if not folder:
+        folder = ""
+    granule_name = match.group(2)
+
+    if cloud:
+        # build the full filename of the .tif file we want
+        # 'vsizip' tells GDAL that we are working with a .zip file
+        # 'vsis3' tells GDAL that the file is hosted in an s3 bucket
+        # return ("/vsizip/vsis3/" + bucket + "/" + folder + granule_name + ".zip/" + granule_name
+        #         + "/" + granule_name + "_" + polarization + ".tif")
+        return os.path.join(
+            "/vsizip/vsis3/", bucket, folder, granule_name + ".zip/", granule_name, 
+            granule_name + "_" + polarization + ".tif"
+        )
+    else:
+        return os.path.join(
+            "/vsizip/" + local_path, granule_name + ".zip/", granule_name, 
+            granule_name + "_" + polarization + ".tif"
+        )
+
+def gdal_build_vrt(filename, tif_list):
+    # TODO: Move these static variables to global?
+    pixfun_name = 'average'
+    pixfun = """
+import numpy as np
+def average(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize, buf_radius, gt, **kwargs):
+    div = np.zeros(in_ar[0].shape)
+    for i in range(len(in_ar)):
+        div += (in_ar[i] != 0)
+    div[div == 0] = 1
+    out_ar[:] = np.sum(in_ar, axis=0, dtype='double') / div
+"""
+
+    contents = f"""    <PixelFunctionType>{pixfun_name}</PixelFunctionType>
+        <PixelFunctionLanguage>Python</PixelFunctionLanguage>
+        <PixelFunctionCode><![CDATA[{pixfun}]]>
+        </PixelFunctionCode>
+    """
+
+    cmd = (f'gdalbuildvrt -overwrite '
+          f'{filename} {" ".join(tif_list)}')
+    subprocess.check_call(cmd, shell=True)
+
+    with open(filename) as f:
+        lines = f.readlines()
+
+    lines[3] = lines[3].replace('band="1"',
+                                'band="1" subClass="VRTDerivedRasterBand"')
+
+    lines.insert(4, contents)
+
+    with open(filename, 'w') as f:
+        f.writelines(lines)
+
+""" 
+Blocks the thread until we can confirm that filename exists.
+Used to prevent a race condition in build_vrt.
+"""
+def block_until_file_created(filename, max_attempts=3):
+    attempts = 0
+    
+    while attempts < max_attempts:
+        if os.path.isfile(filename):
+            # exit the function
+            return None
+        attempts += 1
+        time.sleep(5)
+        
+    # if we exceed max_attempts and file still not found
+    # TODO: Raise FileNotFound Error
+    raise RuntimeError("{} not found".format(filename))
+
+def calc_temp_avg_and_upload_to_s3(s3, dest_bucket, prefix_str, year, path_frame, VV_VRT_filename, VH_VRT_filename):
+    print("VV...", end=None)
+    vv_tif_filename = calc_temp_avg(VV_VRT_filename)
+    vv_tif_s3_key = os.path.join(prefix_str, year, path_frame, vv_tif_filename)
+    upload_to_s3(s3, vv_tif_filename, dest_bucket, vv_tif_s3_key)
+    print("DONE")
+
+    print("VH...", end=None)
+    vh_tif_filename = calc_temp_avg(VH_VRT_filename)
+    vh_tif_s3_key = os.path.join(prefix_str, year, path_frame, vh_tif_filename)
+    upload_to_s3(s3, vh_tif_filename, dest_bucket, vh_tif_s3_key)
+    print("DONE")
+
+    # after uploading VRTs and averaging, delete them from local filesystem
+    os.remove(VV_VRT_filename)
+    os.remove(VH_VRT_filename)
+
+    # after uploading .tifs, delete them from local filesystem
+    os.remove(vv_tif_filename)
+    os.remove(vh_tif_filename)
+
+def calc_temp_avg(vrt_filename):
+    tif_filename = os.path.splitext(ntpath.basename(vrt_filename))[0] + ".tif"
+    cmd = (f'gdal_translate --config '
+          f'GDAL_VRT_ENABLE_PYTHON YES '
+          f'{vrt_filename} {tif_filename}')
+    subprocess.check_call(cmd, shell=True)
+
+    return tif_filename
+
+def upload_to_s3(s3, local_filepath, bucket, key):
+    try:
+        s3.meta.client.upload_file(Filename=local_filepath, Bucket=bucket, Key=key)
+    except ClientError as e:
+        # TODO: add descriptive error messages
+        print("ERROR!", e)
+
+
+if __name__ == '__main__':
+   main()
--- data-prep/Sentinel/automate/readme.md
+++ data-prep/Sentinel/automate/readme.md
@@ -1,20 +1,24 @@
-This folder contains code to automate the processing of sentinel granules using the Hyp3 API and the copying of said granules to an s3 bucket.
+This folder contains code to automate the processing of sentinel granules using the Hyp3 API, the copying of said granules to an s3 bucket, the creation of virtual rasters for each year/path/frame, and temporal average for each year/path/frame.
 
-[test_automation.py](test_automation.py) should be run in the same directory as a config.ini file, which should be structured as such:
+Create a conda virtual environment with environment.yml to install the dependencies the script needs to run. 
+
+[automation.py](automation.py) should be run in the same directory as a config.ini file, which should be structured as such:
 
 ```
 [HyP3]
 username = <username>
 password = <password>
 
-[s3]
-prefix_str = <prefix_str>
+[aws]
+aws_access_key_id = <aws_access_key_id>
+aws_secret_access_key = <aws_secret_access_key>
 dest_bucket = <dest_bucket>
+prefix_str = <prefix_str>
 
 [csv]
 csv = <csv>
 
 [misc]
-max_threads = <max_threads>
+max_threads = 4
 
 ```
