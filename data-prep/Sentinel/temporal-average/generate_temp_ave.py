d87294467134dbfee4ad78bca7445cbb532e814b
Richard H. Chen <richard.h.chen@jpl.nasa.gov>
2021-04-27 15:47:50 -0700

    Added dry_season_only switch for calculating temporal average

--- data-prep/Sentinel/temporal-average/generate_temp_ave.py
+++ data-prep/Sentinel/temporal-average/generate_temp_ave.py
@@ -1,34 +1,54 @@
-from pathlib import Path
 import subprocess
+from datetime import datetime
+from pathlib import Path
+
+state = 'ucayali'
+year = 2020
+path = 171
+frame = 617
+dry_season_only = True
 
 # Directory storing the Sentinel-1 images
-data_dir = Path('S1-171-617-2020')
+data_dir = Path(f'{state}/{year}/{path}-{frame}')
 
-# Search **recursively** all VV images under "data_dir"
-# If you don't want the recursive search, change "rglob" to "glob"
-tifs = [str(tif) for tif in sorted(data_dir.rglob('*_VV.tif'))]
+out_tif_list = []
+for polarization in ['VV', 'VH']:
+    tif_list = []
+    for zip_file in sorted(data_dir.glob('*.zip')):
+        granule_name = zip_file.stem
+        acquisition_time = datetime.strptime(zip_file.name.split('_')[2],
+                                             '%Y%m%dT%H%M%S')
+        tif = (f'/vsizip/{data_dir}/{zip_file.name}/'
+               f'{granule_name}/{granule_name}_{polarization}.tif')
+        if dry_season_only:
+            if (acquisition_time.month >= 5) & (acquisition_time.month <= 9):
+                tif_list.append(tif)
+        else:
+            tif_list.append(tif)
 
-# Output VRT file
-vrt = data_dir / 'vv.vrt'
+    # Output VRT file
+    vrt = data_dir / f'{state}_{path}-{frame}_{year}_{polarization}.vrt'
 
-# Output GeoTIFF file
-out_tif = data_dir / 'vv.tif'
+    # Output GeoTIFF file
+    out_tif = vrt.with_suffix('.tif')
 
-############################## Build initial VRT ##############################
-cmd = (f'gdalbuildvrt '
-       f'-overwrite '
-       f'{vrt} {" ".join(tifs)}')
-subprocess.check_call(cmd, shell=True)
+    if dry_season_only:
+        vrt = vrt.with_suffix('.dry.vrt')
+        out_tif = out_tif.with_suffix('.dry.tif')
 
-####################### Insert pixel function into VRT ########################
-with open(vrt) as f:
-    lines = f.readlines()
+    cmd = (f'gdalbuildvrt '
+           f'-overwrite '
+           f'{vrt} {" ".join(tif_list)}')
+    subprocess.check_call(cmd, shell=True)
 
-lines[3] = lines[3].replace('band="1"',
-                            'band="1" subClass="VRTDerivedRasterBand"')
+    with open(vrt) as f:
+        lines = f.readlines()
 
-pixfun_name = 'average'
-pixfun = """
+    lines[3] = lines[3].replace('band="1"',
+                                'band="1" subClass="VRTDerivedRasterBand"')
+
+    pixfun_name = 'average'
+    pixfun = """
 import numpy as np
 def average(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize, buf_radius, gt, **kwargs):
   div = np.zeros(in_ar[0].shape)
@@ -38,20 +58,33 @@ def average(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize,
   out_ar[:] = np.sum(in_ar, axis=0, dtype='double') / div
 """
 
-contents = f"""    <PixelFunctionType>{pixfun_name}</PixelFunctionType>
+    contents = f"""    <PixelFunctionType>{pixfun_name}</PixelFunctionType>
     <PixelFunctionLanguage>Python</PixelFunctionLanguage>
     <PixelFunctionCode><![CDATA[{pixfun}]]>
     </PixelFunctionCode>
 """
 
-lines.insert(4, contents)
+    lines.insert(4, contents)
 
-with open(vrt, 'w') as f:
-    f.writelines(lines)
+    with open(vrt, 'w') as f:
+        f.writelines(lines)
 
-########################## Translate VRT to GeoTIFF ###########################
-cmd = (f'gdal_translate '
+    cmd = (f'gdal_translate '
+           f'-co COMPRESS=LZW '
+           f'--config GDAL_VRT_ENABLE_PYTHON YES '
+           f'{vrt} {out_tif}')
+    # subprocess.check_call(cmd, shell=True)
+
+    out_tif_list.append(out_tif)
+
+g0_tif = data_dir / f'{state}_{path}-{frame}_{year}_gamma0.tif'
+if dry_season_only:
+    g0_tif = g0_tif.with_suffix('.dry.tif')
+cmd = (f'gdal_merge.py '
+       f'-init "0 0 0" '
+       f'-separate '
        f'-co COMPRESS=LZW '
-       f'--config GDAL_VRT_ENABLE_PYTHON YES '
-       f'{vrt} {out_tif}')
-subprocess.check_call(cmd, shell=True)
+       f'-o {g0_tif} '
+       f'{out_tif_list[0]} '
+       f'{out_tif_list[1]}')
+subprocess.call(cmd, shell=True)
