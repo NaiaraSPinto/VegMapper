0425271056b999208f39c0bcdbc09fb9de2d0ff6
lemarpopal <19434803+lemarpopal@users.noreply.github.com>
2021-08-23 22:19:59 -0700

    clean up code and add comments

--- data-prep/Sentinel/automate/sentinel_submit_and_copy.py
+++ data-prep/Sentinel/automate/sentinel_submit_and_copy.py
@@ -1,51 +1,55 @@
 from hyp3_sdk import HyP3
 
 import pandas as pd
-import numpy as np
-import rasterio
 
 import boto3
-from botocore.exceptions import ClientError
 
 import getpass
 import argparse
 import os
-import sys
 import traceback
-import re
 import ntpath
-import time
-import subprocess
 from datetime import datetime,timezone
-from copy import copy
-from pathlib import Path
 
 # TODO: Use global variables for things like s3, bucket_name?
 
 def main():
+    ### Setup parsing
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('--bucket_path', type=str, default='servir-public/geotiffs/peru/sentinel_1', help='Enter S3 bucket path to store processed granules (e.g. servir-public/geotiffs/peru/sentinel_1)')
+    parser.add_argument('--bucket_path', type=str, help='Enter S3 bucket path to store processed granules (e.g. servir-public/geotiffs/peru/sentinel_1)')
     parser.add_argument('--csv', type=str, help='Path to CSV file that contains granules to be submitted or copied.', required=True)
     parser.add_argument('-h', '--help', action='help', help='Display help information.')
     args = parser.parse_args()
+    ###
 
+    ### Get HyP3 credentials and authenticate
     hyp3_username = input("Enter HyP3 Username: ")
     hyp3_password = getpass.getpass("Enter HyP3 Password: ")
     hyp3 = HyP3(username=hyp3_username, password=hyp3_password)
+    ###
 
-    dest_bucket = args.bucket_path.split("/")[0]
-    prefix_str = args.bucket_path.split("/", 1)[1]
-    copy_processed_granules_to_bucket = True if args.bucket_path else False
-
+    ### Connect to S3. Assumes S3 role already set up in EC2
     try:
         s3 = boto3.resource('s3')
     except:
         print("Error connecting to S3. Make sure your EC2 instance is able to access S3.")
+    ###
 
+    ### Convenient variables
+    dest_bucket = args.bucket_path.split("/")[0]
+    prefix_str = args.bucket_path.split("/", 1)[1]
+    copy_processed_granules_to_bucket = True if args.bucket_path else False
+    ###
+
+    ### Dictionary keyed by year_path_frame (e.g. '2018_25_621'), value is list of granules names for that particular year_path_fame
     granules_group_dict = generate_granules_group_dict(args.csv)
+    ###
 
+    ### Submit granules
     submit_granules(hyp3, granules_group_dict)
+    ###
     
+    ### Loop through and copy to bucket, or if no bucket specified, show user which ASF bucket granule is in
     for year_path_frame in granules_group_dict.keys():
         year, path_frame = year_path_frame.split('_', 1)
         granule_sources = get_granule_sources(hyp3, year, path_frame)
@@ -64,6 +68,7 @@ def main():
             for copy_source in granule_sources:
                 print(f"{copy_source['bucket']}/{copy_source['key']}")
                 print(f"Expiration Time: {copy_source['expiration_time']}")
+    ###
 
     print("Done with everything.")
 
@@ -74,7 +79,7 @@ def generate_granules_group_dict(csv_path):
     granules_df = pd.read_csv(csv_path)
     granules_df['Year'] = granules_df['Acquisition Date'].apply(lambda x: x.split('-')[0])
     granules_df = granules_df.filter(['Granule Name','Year','Path Number','Frame Number'])
-    granules_df['year_path_frame'] = granules_df.apply(lambda row: "{}_{}_{}".format(row['Year'], row['Path Number'], row['Frame Number']), axis=1)
+    granules_df['year_path_frame'] = granules_df.apply(lambda row: f"{row['Year']}_{row['Path Number']}_{row['Frame Number']}", axis=1)
 
     granules_groups = granules_df.groupby(by=['year_path_frame'])['Granule Name']
 
@@ -94,11 +99,11 @@ Submits the granules to Hyp3.
 def submit_granules(hyp3, granules_group_dict):
     print("You will be submitting the following granules for processing:")
     for year_path_frame,granule_name_list in granules_group_dict.items():
-        print("{} - {} granules".format(year_path_frame, len(granule_name_list)))
+        print(f"{year_path_frame} - {len(granule_name_list)} granules")
     user_input = input((
         "Enter 'Y' to confirm you would like to submit these granules, "
         "or 'N' if you have already submitted the granules and want to copy the processed granules to your bucket: "))
-    if user_input.lower() != "y":
+    if user_input.lower() != 'y':
         return
 
     # submit the jobs for all year_path_frame's
@@ -112,7 +117,7 @@ def submit_granules(hyp3, granules_group_dict):
 
 def get_granule_sources(hyp3, year, path_frame):
     # returns a list of dictionaries where files are located on ASF's bucket
-    year_path_frame = "_".join([year, path_frame])
+    year_path_frame = '_'.join([year, path_frame])
 
     batch = hyp3.find_jobs(name=year_path_frame)
     batch = hyp3.watch(batch)
@@ -133,11 +138,9 @@ def copy_granules_to_bucket(s3, dest_bucket, prefix_str, year, path_frame, granu
                 print("\nError copying processed granule to your bucket. Traceback:")
                 print(traceback.print_exc())
                 print(f"Failed granule: {copy_source}")
-
         else:
             # job is expired and cannot be copied
             pass
 
 if __name__ == '__main__':
    main()
-
