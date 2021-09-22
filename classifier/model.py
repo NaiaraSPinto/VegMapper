7aded035eb31c8791c2c930ca9016b8da1ac6023
Richard H. Chen <richard.h.chen@jpl.nasa.gov>
2021-09-22 15:06:53 -0700

    Update model.py to enable looping over tiles

--- classifier/model.py
+++ classifier/model.py
@@ -1,9 +1,12 @@
 import subprocess
+import sys
 from pathlib import Path
+from urllib.parse import urlparse
 
 # User inputs
-stack = '/vsis3/servir-stacks/ucayali/2020/all-bands/ucayali_stacks_2020_h1v1.tif'
-out_tif = '/vsis3/servir-stacks/ucayali/2020/test.tif'
+site = 'ucayali'
+year = 2020
+version = 'v1.0'    # model results will be saved under s3://servir-stacks/site/year/model/version/
 
 # Model parameters
 intercept = 24.199260536
@@ -11,10 +14,7 @@ posteriors = [-27.817542580, 11.724057104, 0.002757478, -17.933519020,
               -18.064926884, -0.012282095, 30.887101573, -0.037257098,
               -9.236119539, -47.321276739]
 
-# Temporary VRT to apply the model onto stack
-vrt = Path('tmp.vrt')
-
-# Pixel function for applying model
+# Pixel function of model
 pixfun_name = 'model'
 pixfun = f"""
 import numpy as np
@@ -39,36 +39,49 @@ contents = f"""    <PixelFunctionType>{pixfun_name}</PixelFunctionType>
     </PixelFunctionCode>
 """
 
-# Build VRT
-cmd = (f'gdalbuildvrt -overwrite -b 1 '
-       f'{vrt} ')
-for i in range(8):
-    cmd = cmd + f' {stack}'
-subprocess.check_call(cmd, shell=True)
+bucket = 'servir-stacks'
+cmd = f'gsutil ls s3://{bucket}/{site}/{year}/all-bands/*.tif'
+stack_url_list = subprocess.check_output(cmd, shell=True).decode(sys.stdout.encoding).splitlines()
 
-# Read VRT
-with open(vrt) as f:
-    lines = f.readlines()
+for i, stack_url in enumerate(stack_url_list):
+    print(f'\n({i+1}/{len(stack_url_list)}) Applying model to {stack_url} ...')
+    stack_key = urlparse(stack_url).path
+    stack_tif = f'/vsis3/{bucket}{stack_key}'
+    model_tif = stack_tif.replace('all-bands', f'model/{version}').replace(f'{site}_stacks', f'{site}_model')
 
-# Insert pixel function
-lines[3] = lines[3].replace('band="1"',
-                            'band="1" subClass="VRTDerivedRasterBand"')
-for i in range(8):
-    lines[8+i*8] = lines[8+i*8].replace(f'<SourceBand>1</SourceBand>',
-                                        f'<SourceBand>{i+1}</SourceBand>')
-lines.insert(4, contents)
+    # Temporary VRT to apply the model onto stack
+    vrt = Path('tmp.vrt')
 
-# Write VRT
-with open(vrt, 'w') as f:
-    f.writelines(lines)
+    # Build VRT
+    cmd = (f'gdalbuildvrt -overwrite -b 1 '
+        f'{vrt} ')
+    for i in range(8):
+        cmd = cmd + f' {stack_tif}'
+    subprocess.check_call(cmd, shell=True)
 
-# Translate VRT into GeoTIFF
-cmd = (f'gdal_translate --config '
-       f'GDAL_VRT_ENABLE_PYTHON YES '
-       f'-co compress=lzw '
-       f'--config CPL_VSIL_USE_TEMP_FILE_FOR_RANDOM_WRITE YES '
-       f'{vrt} {out_tif}')
-subprocess.check_call(cmd, shell=True)
+    # Read VRT
+    with open(vrt) as f:
+        lines = f.readlines()
 
-# Delete VRT
-vrt.unlink()
+    # Insert pixel function
+    lines[3] = lines[3].replace('band="1"',
+                                'band="1" subClass="VRTDerivedRasterBand"')
+    for i in range(8):
+        lines[8+i*8] = lines[8+i*8].replace(f'<SourceBand>1</SourceBand>',
+                                            f'<SourceBand>{i+1}</SourceBand>')
+    lines.insert(4, contents)
+
+    # Write VRT
+    with open(vrt, 'w') as f:
+        f.writelines(lines)
+
+    # Translate VRT into GeoTIFF
+    cmd = (f'gdal_translate --config '
+        f'GDAL_VRT_ENABLE_PYTHON YES '
+        f'-co compress=lzw '
+        f'--config CPL_VSIL_USE_TEMP_FILE_FOR_RANDOM_WRITE YES '
+        f'{vrt} {model_tif}')
+    subprocess.check_call(cmd, shell=True)
+
+    # Delete VRT
+    vrt.unlink()
