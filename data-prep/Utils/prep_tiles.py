1e025bc093d05d1ef89ddea8597e8641186c13ac
Remy Wolf <remywolf.r@gmail.com>
2021-12-14 13:01:39 -0800

    removed admshp option from prep-tiles.py

--- data-prep/Utils/prep_tiles.py
+++ data-prep/Utils/prep_tiles.py
@@ -19,25 +19,6 @@ def get_utm_zone(lat, lon):
     return utm_zone, utm_epsg
 
 
-def extract_boundary(adm1_shp, state):
-    # Extract state boundary
-    gdf_adm = gpd.read_file(adm1_shp)
-    for col in gdf_adm.columns:
-        if re.fullmatch(r'ADM1_[A-Z][A-Z]', col):
-            adm1_col = col
-            print(adm1_col)
-    gdf_adm.crs = 'epsg:4326'
-    for i in gdf_adm.index:
-        gdf_adm.loc[i, adm1_col] = unidecode(gdf_adm.loc[i, adm1_col]).lower()
-    idx = gdf_adm.index[gdf_adm.loc[:, adm1_col] == state]
-    gdf_boundary = gdf_adm.iloc[idx, :].reset_index(drop=True)
-    shp_boundary = f'{state}_boundary.geojson'
-    gdf_boundary.to_file(shp_boundary, driver='GeoJSON')
-    print(f'Boundary for {state.capitalize()}: {shp_boundary}')
-
-    return shp_boundary
-
-
 def get_tiles(shp_boundary, t_size, aoi_name):
     gdf_boundary = gpd.read_file(shp_boundary)
 
@@ -94,24 +75,16 @@ def main():
     parser.add_argument('aoi_name', metavar='aoi_name',
                         type=str,
                         help='name of area of interest (AOI)')
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--aoishp',
+    parser.add_argument('aoishp',
                        metavar='aoishp',
                        type=Path,
                        help='shp/geojson of AOI')
-    group.add_argument('--admshp',
-                       metavar='admshp',
-                       type=Path,
-                       help='shp/geojson of subnational administrative boundaries')
     parser.add_argument('t_size', metavar='tile_size',
                         type=int,
                         help=('tile size in meters'))
     args = parser.parse_args()
 
-    if args.admshp:
-        shp_boundary = extract_boundary(args.admshp, args.aoi_name.lower())
-    else:
-        shp_boundary = args.aoishp
+    shp_boundary = args.aoishp
 
     get_tiles(shp_boundary, args.t_size, args.aoi_name.lower())
 
