1abf4727a15b4bc408b7809888d801d9761d5611
Richard H. Chen <richard.oy.chen@gmail.com>
2022-10-07 10:57:56 -0700

    Update S1 codes and notebook (#69)
    * Add ALOS-2 notebook and correct a bug in alos2.proc_tarfiles
    
    * Update S1 and ALOS-2 notebooks
    
    * Update S1 codes
    
    * Avoid PathURL used in updating s1_proc.json
    
    * Not saving s1_proc.json for now
    
    * Update S1 notebook

--- vegmapper/s1/postprocess.py
+++ vegmapper/s1/postprocess.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python
 
-import argparse
-import re
+import json
 import subprocess
-import sys
 from datetime import datetime
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
+import rasterio
 
-import vegmapper
 from vegmapper import pathurl
-from vegmapper.pathurl import ProjDir
 
-
-pf_pattern = re.compile(r'^\d+_\d+$', re.ASCII)
-
-# RTC product filename pattern
-rtc_zip_pattern = re.compile(r'^\w{3}_\w{2}_\d{8}T\d{6}_\w{3}_RTC\d{2}_\w{1}_\w{6}_\w{4}.zip$')
 
 # GeoTIFF suffix of data layer in the RTC product
 layer_suffix = {
@@ -27,9 +20,19 @@ layer_suffix = {
     'LS': 'ls_map',
 }
 
+# Expect this from user before processing
+# s1_proc = {
+#     's1_dir': proj_dir / 'Sentinel-1',
+#     'start_date': '2021-01-01',
+#     'end_date': '2021-12-31',
+#     'frames': None,
+# }
 
-def build_vrt(proj_dir: ProjDir, start_date, end_date, layers=['VV', 'VH', 'INC', 'LS'], path_frame=None):
-    s1_dir = proj_dir / 'Sentinel-1'
+
+def get_rtc_products(s1_proc):
+    s1_dir = s1_proc['s1_dir']
+    start_date = s1_proc['start_date']
+    end_date = s1_proc['end_date']
 
     # Read rtc_products.csv
     rtc_products_file = s1_dir / 'rtc_products.csv'
@@ -44,71 +47,102 @@ def build_vrt(proj_dir: ProjDir, start_date, end_date, layers=['VV', 'VH', 'INC'
     df_products['stopTime']= pd.to_datetime(df_products['stopTime'])
     df_products = df_products[(dt_start < df_products.startTime) & (df_products.startTime < dt_end)]
 
-    # Get list of path_frame
+    # Get dictionary of vsi_paths of all RTC products
+    # {frame: {'paths': [vsi_paths]}
     if s1_dir.is_cloud:
         vsi_prefix = f'/vsizip/vsi{s1_dir.storage}/{s1_dir.bucket}/{s1_dir.prefix}'
     else:
         vsi_prefix = f'/vsizip/{s1_dir}'
     gb = df_products.groupby(['pathNumber', 'frameNumber'])
-    file_paths = {
-        f'{p}_{f}': [f'{vsi_prefix}/{p}_{f}/{filename}'for filename in gb.get_group((p, f)).filename.to_list()] for p, f in gb.groups.keys()
+    product_paths = {
+        f'{p}_{f}': {'rtc_products': [f'{vsi_prefix}/{p}_{f}/{filename}'for filename in gb.get_group((p, f)).filename.to_list()]} for p, f in gb.groups.keys()
     }
 
-    if path_frame is not None:
-        file_paths = {path_frame: file_paths[path_frame]}
+    # Update s1_proc
+    if s1_proc['frames'] is not None:
+        frames = s1_proc['frames']
+        s1_proc['frames'] = {}
+        if isinstance(frames, str):
+            frames = [frames]
+        s1_proc['frames'] = {frame_id: product_paths[frame_id] for frame_id in frames}
+    else:
+        s1_proc['frames'] = product_paths
+    # with open(f'{s1_dir}/s1_proc.json', 'w') as f:
+    #     json.dump(s1_proc, f)
 
-    for path_frame, vsi_paths in file_paths.items():
+
+def build_vrt(s1_proc, layers=['VV', 'VH', 'INC', 'LS'], quiet=True):
+    s1_dir = s1_proc['s1_dir']
+    start_date = s1_proc['start_date']
+    end_date = s1_proc['end_date']
+
+    for frame_id, frame_dict in s1_proc['frames'].items():
+        print(f'Building VRTs for {frame_id}')
         for layer in layers:
+            # Get vsi paths of RTC products
             tif_list = []
-            for vsi_path in vsi_paths:
+            for vsi_path in frame_dict['rtc_products']:
                 zip_stem = Path(vsi_path).stem
-                tif_list.append(f'{vsi_path}/{zip_stem}/{zip_stem}_{layer}.tif')
-
+                tif_list.append(f'{vsi_path}/{zip_stem}/{zip_stem}_{layer_suffix[layer]}.tif')
             # Build VRT
             if s1_dir.is_cloud:
-                vrt = f'/vsi{s1_dir.storage}/{s1_dir.bucket}/{s1_dir.prefix}/{path_frame}/{start_date}_{end_date}/{layer}.vrt'
+                vrt = f'/vsi{s1_dir.storage}/{s1_dir.bucket}/{s1_dir.prefix}/{frame_id}/{start_date}_{end_date}/{layer}.vrt'
             else:
-                vrt = Path(f'{s1_dir}/{path_frame}/{start_date}_{end_date}/{layer}.vrt')
+                vrt = Path(f'{s1_dir}/{frame_id}/{start_date}_{end_date}/{layer}.vrt')
                 if not vrt.parent.exists():
                     vrt.parent.mkdir(parents=True)
-            print(f'\nBuilding {layer} VRT for {path_frame} to {vrt}')
             cmd = f'gdalbuildvrt -overwrite {vrt} {" ".join(tif_list)}'
+            if quiet:
+                cmd = cmd + ' -q'
             subprocess.check_call(cmd, shell=True)
+            # Update s1_proc
+            s1_proc['frames'][frame_id][layer] = {'vrt': vrt}
+
+    # with open(f'{s1_dir}/s1_proc.json', 'w') as f:
+    #     json.dump(s1_proc, f)
 
 
-def s1_proc(proj_dir, year, m1, m2, path_frame=None):
-    # Get list of path_frame
-    path_frame_list = []
-    if path_frame is not None:
-        if pf_pattern.fullmatch(path_frame):
-            path_frame_list.append(path_frame)
-        else:
-            raise Exception(f'{path_frame} is not of correct path_frame format.')
-    else:
-        p = ProjDir(proj_dir)
-        if p.is_cloud:
-            ls_cmd = f'gsutil ls {proj_dir}/sentinel_1/{year}'
-            obj_list = subprocess.check_output(ls_cmd, shell=True).decode(sys.stdout.encoding).splitlines()
-            for obj_path in obj_list:
-                obj_name = Path(obj_path).name
-                if obj_path[-1] == '/' and pf_pattern.fullmatch(obj_name):
-                    path_frame_list.append(obj_name)
-        else:
-            for p in (proj_dir / f'sentinel_1/{year}').iterdir():
-                if pf_pattern.match(p.name):
-                    path_frame_list.append(p.name)
+def calc_temporal_mean(s1_proc, layers=['VV', 'VH', 'INC', 'LS'], quiet=True):
+    s1_dir = s1_proc['s1_dir']
+    start_date = s1_proc['start_date']
+    end_date = s1_proc['end_date']
 
-    for path_frame in path_frame_list:
-        print(f'\nProcessing {year}_{path_frame} ...')
-
-        for layer in ['VV', 'VH', 'INC', 'LS']:
-            # Build VRT
-            subprocess.check_call(f's1_build_vrt.py {proj_dir}/sentinel_1 {year}_{path_frame} {layer} --m1 {m1} --m2 {m2}', shell=True)
+    for frame_id, frame_dict in s1_proc['frames'].items():
+        print(f'Calculating temporal means for {frame_id}')
+        for layer in layers:
             # Calculate temporal mean
-            vrtpath = f'{proj_dir}/sentinel_1/{year}/{path_frame}/{year}_{path_frame}_{layer}.vrt'
-            subprocess.check_call(f'calc_vrt_stats.py {vrtpath} mean', shell=True)
+            vrt = frame_dict[layer]['vrt']
+            subprocess.check_call(f'calc_vrt_stats.py {vrt} mean', shell=True)
+            # Update s1_proc
+            frame_dict[layer]['mean'] = s1_dir / frame_id / f'{start_date}_{end_date}' / f'{layer}_mean.tif'
 
-        # Remove left and right edge pixels
-        subprocess.check_call(f's1_remove_edges.py {proj_dir}/sentinel_1/{year}/{path_frame}', shell=True)
+    # with open(f'{s1_dir}/s1_proc.json', 'w') as f:
+    #     json.dump(s1_proc, f)
 
-        print(f'\nDone processing {year}_{path_frame}.')
+
+def remove_edges(s1_proc, edge_depth=200):
+    for frame_id, frame_dict in s1_proc['frames'].items():
+        print(f'Removing {edge_depth} edge pixels on the left and right for {frame_id}')
+        vv = frame_dict['VV']['mean'].path
+        vv_edge_removed = Path('VV_mean.tif')
+        ls = frame_dict['LS']['mean'].path
+        subprocess.check_call((f'remove_edges.py {vv} {vv_edge_removed} '
+                               f'--maskfile {ls} '
+                               f'--lr_only --edge_depth {edge_depth}'),
+                               shell=True)
+        with rasterio.open(vv_edge_removed) as dset:
+            mask = dset.read_masks(1)
+        for layer in ['VV', 'VH', 'INC']:
+            tif = frame_dict[layer]['mean'].path
+            tif_edge_removed = Path(f'{layer}_mean.tif')
+            if layer != 'VV':
+                with rasterio.open(tif) as dset:
+                    data = dset.read(1)
+                    if layer == 'INC':
+                        data = np.rad2deg(data)
+                    data[mask == 0] = dset.nodata
+                    profile = dset.profile
+                with rasterio.open(tif_edge_removed, 'w', **profile) as dset:
+                    dset.write(data, 1)
+            pathurl.copy(tif_edge_removed, tif, overwrite=True)
+            tif_edge_removed.unlink()
